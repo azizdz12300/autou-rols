fcb60564b6fb68e616748e6e3a6797a0f7eafec1
azizdz12300 <44880483+azizdz12300@users.noreply.github.com>
2019-03-13 12:48:59 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,85 +1,36 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
-
-const client = new Discord.Client();
-let ar = JSON.parse(fs.readFileSync(`./Data/AutoRole.json`, `utf8`))
-
-const prefix = "";//البريفكس
-
-client.on('guildMemberAdd', member => {
-  if(!ar[member.guild.id]) ar[member.guild.id] = {
-  onoff: 'Off',
-  role: 'Member'
-  }
-  if(ar[member.guild.id].onoff === 'Off') return;
-member.addRole(member.guild.roles.find(`name`, ar[member.guild.id].role)).catch(console.error)
-})
-
+const rWlc = {}
 client.on('message', message => { 
-  var whitelisted = ""// ايديك
-  var sender = message.author
-
-if(!message.guild) return
-  if(!ar[message.guild.id]) ar[message.guild.id] = {
-  onoff: 'Off',
-  role: 'Member'
+var prefix = "-";//البرفكس  
+if(message.channel.type === "dm") return;
+if(message.author.bot) return;
+   if(!rWlc[message.guild.id]) rWlc[message.guild.id] = { 
+    role: "member"
+  }
+const channel = rWlc[message.guild.id].role
+  if (message.content.startsWith(prefix + "autorole")) { 
+    if(!message.member.hasPermission(`MANAGE_GUILD`)) return;
+    let newrole = message.content.split(' ').slice(1).join(" ") 
+    if(!newrole) return message.reply(`**${prefix}autorole Role Name**`) 
+    rWlc[message.guild.id].role = newrole
+    message.channel.send(`**${message.guild.name}'s Role Has been changed to ${newrole}**`); 
   }
 
-if(message.content.startsWith(prefix + `autorole`)) {
-         if(whitelisted.includes(sender.id)) {
-  let perms = message.member.hasPermission(`MANAGE_ROLES`)
 
-  if(!perms) return message.reply(`You don't have permissions, required permission : Manage Roles.`)
-  let args = message.content.split(" ").slice(1)
-  if(!args.join(" ")) return message.reply(`${prefix}autorle toggle/setrole [ROLE NAME]`)
-  let state = args[0]
-  if(!state.trim().toLowerCase() == 'toggle' || !state.trim().toLowerCase() == 'setrole') return message.reply(`Please type a right state, ${prefix}modlogs toggle/setrole [ROLE NAME]`) 
-    if(state.trim().toLowerCase() == 'toggle') { 
-     if(ar[message.guild.id].onoff === 'Off') return [message.channel.send(`**The Autorole Is __𝐎𝐍__ !**`), ar[message.guild.id].onoff = 'On']
-     if(ar[message.guild.id].onoff === 'On') return [message.channel.send(`**The Autorole Is __𝐎𝐅𝐅__ !**`), ar[message.guild.id].onoff = 'Off']
-    }
-   if(state.trim().toLowerCase() == 'set') {
-   let newRole = message.content.split(" ").slice(2).join(" ")
-   if(!newRole) return message.reply(`${prefix}autorole setrole [ROLE NAME]`)
-     if(!message.guild.roles.find(`name`,newRole)) return message.reply(`I Cant Find This Role.`)
-    ar[message.guild.id].role = newRole
-     message.channel.send(`**The AutoRole Has Been Changed to ${newRole}.**`)
-   } 
-         }
+client.on("guildMemberAdd", member => {
+      if(!rWlc[member.guild.id]) rWlc[member.guild.id] = {
+    role: "member"
   }
+  const Role = rWlc[member.guild.id].role
+    const sRole = rWlc[member.guild.id].role
+    let Rrole = member.guild.roles.find('name', sRole); 
+  member.addRole(Rrole); 
  
-if(message.content === prefix + 'info') {
-    let perms = message.member.hasPermission(`MANAGE_GUILD`) 
-    if(!perms) return message.reply(`You don't have permissions.`)
-    var embed = new Discord.RichEmbed()
-
-.addField(`Autorole : :sparkles:  `, `
-
-State : __${ar[message.guild.id].onoff}__
-Role : __${ar[message.guild.id].role}__`)
-
-
-    .setColor(`BLUE`)
-    message.channel.send({embed})
-  }
-
-
-    fs.writeFile("./Data/AutoRole.json", JSON.stringify(ar), (err) => {
-    if (err) console.error(err)
-  });
-
-
-})
-
-
-
-client.on('ready', () => {
-  console.log(`AutoRole Code Started By Friends Team`);
-    client.user.setStatus("dnd")
-});
-
-
-
+      
+      
+      }); 
+}); 
 
 client.login(process.env.BOT_TOKEN);// لا تغير فيها شيء
